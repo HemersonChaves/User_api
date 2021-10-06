424404b06022d62c44d6cbdd082b88f7e4553629
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-06 15:58:52 -0300

    refactor(creatuser): ensure  not be able to turn a non existing user as admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -13,7 +13,7 @@ class TurnUserAdminController {
             return response.status(201).send(user);
         } catch (error) {
             return response
-                .status(400)
+                .status(404)
                 .json({ error: "Nao pode criar um usuario" });
         }
     }
