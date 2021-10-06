1f4a0b519a91b692d238103f8682ad0bf559ade9
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-06 09:40:31 -0300

    feat(user): implement of the method handle in ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,18 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const users = this.listAllUsersUseCase.execute();
+            return response.status(201).json(users);
+        } catch (error) {
+            return response
+                .status(500)
+                .json({ error: "Nao pode encontrar nenhum usuario" });
+        }
+    }
 }
 
 export { ListAllUsersController };
